ef336efe539d58676052659830cd74b0a13cbcd9
Zdenek Dolezal <Griperis@outlook.cz>
2024-04-05 08:50:56 +0200

    interm update 2024

--- js/script.js
+++ js/script.js
@@ -1,57 +1,47 @@
-// Author: Zdeněk Doležal 2019-2021
+// Author: Zdeněk Doležal 2019-*
+// Toggle navigation
+const mainNav = document.querySelector("#main-nav");
+const toggleNavigation = document.querySelector("#toggle-navigation");
+const navIcon = document.querySelector("#nav-icon");
 
-let liItems = $(".main-nav ul li.hide-sm");
 
-$(".smooth-scroll").click(function(event) {
-    let offset = $(".main-nav").height();
-    event.preventDefault();
-    let target = $(this).attr("href");
-    let position = 0;
-    if (target == '#') {
-        position = 0;
-    } else {
-        position = $(target).offset().top;
-    }
-    $("html, body").animate({ scrollTop: position - offset}, 500);
+// toggleNavigation.addEventListener("click", () => {
+//     mainNav.classList.toggle("offset-x-500");
+//     navIcon.classList.toggle("fa-bars");
+//     navIcon.classList.toggle("fa-xmark");
+// });
+
+// Cursor background
+const cursor = document.querySelector(".cursor");
+document.addEventListener("mousemove", (e) => {
+    cursor.style.left = e.pageX - window.scrollX + 'px';
+    cursor.style.top = e.pageY - window.scrollY + 'px';
+
 });
 
-// Toggleable menu
-liItems.click(function(event) {
-    toggleMenuItems();
-});
-$("#nav-toggle").click(function(event) {
-    toggleMenuItems();
-});
+// Position indicator
+// const positionIndicator = document.querySelector("#position-indicator");
+// const indicatorContainer = document.querySelector("#nav-list-wrapper");
+// document.addEventListener("scroll", (e) => {
+//     let height = indicatorContainer.offsetHeight;
+//     let relativeHeight = window.scrollY / (scr.scrollHeight - window.innerHeight);
+//     positionIndicator.style.top = (relativeHeight * height) + 'px';
+// });
 
-function toggleMenuItems() {
-    liItems.toggleClass('hide-sm');
+
+const paginationItems = document.querySelectorAll(".pagination-item");
+const paginationBtns = document.querySelectorAll(".pagination-nav-item");
+
+function focusSection(i) {
+    paginationBtns.forEach((btn) => {
+        btn.classList.remove("active");
+    });
+    paginationBtns[i].classList.add("active");
+    paginationItems.forEach((item) => {
+        item.style.display = "none";
+    });
+    let section = paginationItems[i];
+    section.style.display = "block";
 }
 
-let coll = $(".collapsible");
-
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-      this.classList.toggle("active");
-      var content = this.nextElementSibling;
-      if (content.style.maxHeight){
-        content.style.maxHeight = null;
-      } else {
-        content.style.maxHeight = content.scrollHeight + "px";
-      } 
-    });
-  }
-
-let modal = document.getElementById("modal-window");
-let modalImg = document.getElementById("modal-img-content");
-let imgs = document.getElementsByClassName("modal-show");
-for (let i = 0; i < imgs.length; i++) {
-    let img = imgs[i];
-    img.addEventListener("click", function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    });
-}
-modal.addEventListener("click", function() {
-    modal.style.display = "none"
-});
-
+focusSection(0);
